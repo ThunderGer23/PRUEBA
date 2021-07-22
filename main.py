ea35d436791190f9b33be8e4d0ea69812b6ba8f2
ThunderGer <Jaames11@hotmail.com>
2021-07-21 19:24:34 -0500

    update file main.py update for R2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Mi nombre es Arturo y soy un Caballo (Relincha :v)')
 
 
 # Press the green button in the gutter to run the script.
